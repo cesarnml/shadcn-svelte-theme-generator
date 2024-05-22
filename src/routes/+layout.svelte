795d5a10aa1895be9149e40f259b4d46d4b99341
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2024-05-22 18:05:59 +0700

    add DarkModeSwitch

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,10 @@
 <script>
+  import { ModeWatcher } from 'mode-watcher'
+
   import '../app.css'
 </script>
 
+<ModeWatcher />
 <main class="container grid min-h-screen">
   <slot />
 </main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,17 @@
 <script lang="ts">
   import BackgroundForegroundExample from '$lib/components/BackgroundForegroundExample.svelte'
   import CardExample from '$lib/components/CardExample.svelte'
+  import DarkModeSwitch from '$lib/components/DarkModeSwitch.svelte'
   import InputExample from '$lib/components/InputExample.svelte'
   import MutedExample from '$lib/components/MutedExample.svelte'
   import PrimaryExample from '$lib/components/PrimaryExample.svelte'
 </script>
 
 <div>
-  <h1 class="mb-6 py-4 text-xl font-bold uppercase">shadcn-svelte theme generator</h1>
+  <header class="flex items-center justify-between">
+    <h1 class="mb-6 py-4 text-xl font-bold uppercase">shadcn-svelte theme generator</h1>
+    <DarkModeSwitch />
+  </header>
   <div class="flex gap-8">
     <BackgroundForegroundExample />
     <PrimaryExample />
