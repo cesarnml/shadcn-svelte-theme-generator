dfb34977bd7cf3e21690afa88ab41e9e898fcc86
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2024-05-22 14:22:14 +0700

    add CardExample

---
+++ src/lib/components/CardExample.svelte
@@ -0,0 +1,32 @@
+<script>
+  import ReusableCard from '$lib/components/ReusableCard.svelte'
+  import { Input } from '$lib/components/ui/input'
+  import { Label } from '$lib/components/ui/label'
+  import { hexToHSL } from '$lib/utils/hexToHsl'
+
+  let cardForeground = ''
+  let card = ''
+
+  const updateCardColor = () => {
+    document.documentElement.style.setProperty('--card', hexToHSL(card))
+  }
+
+  const updateCardForegroundColor = () => {
+    document.documentElement.style.setProperty('--card-foreground', hexToHSL(cardForeground))
+  }
+</script>
+
+<div>
+  <h2 class="mb-2 font-mono text-lg font-semibold">Card</h2>
+  <ReusableCard />
+  <div class="flex justify-between gap-4">
+    <div class="flex-grow">
+      <Label>Card</Label>
+      <Input type="color" bind:value={card} on:input={updateCardColor} />
+    </div>
+    <div>
+      <Label>Card Foreground</Label>
+      <Input type="color" bind:value={cardForeground} on:input={updateCardForegroundColor} />
+    </div>
+  </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import CardExample from '$lib/components/CardExample.svelte'
   import InputExample from '$lib/components/InputExample.svelte'
   import PrimaryExample from '$lib/components/PrimaryExample.svelte'
 </script>
@@ -7,7 +8,7 @@
   <h1 class="mb-6 py-4 text-xl font-bold uppercase">shadcn-svelte theme generator</h1>
   <div class="flex gap-8">
     <PrimaryExample />
-    <!-- <hr class="my-4 border-b border-gray-200" /> -->
     <InputExample />
+    <CardExample />
   </div>
 </div>
